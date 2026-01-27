ec80a8cf9ae2d06ccbeb6c960cb0cfafa5126c9c
Zahidul Islam <muhammadzahidulislam2222@gmail.com>
2026-01-27 16:57:34 +0600

    update

--- config/database.ts
+++ config/database.ts
@@ -1,38 +1,12 @@
 import path from 'path';
 
-export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-
-  const connections = {
-    postgres: {
-      connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          rejectUnauthorized: false,
-        },
-      },
-      pool: {
-        min: env.int('DATABASE_POOL_MIN', 2),
-        max: env.int('DATABASE_POOL_MAX', 10),
-      },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
+export default ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      // This forces it to look in your project root -> .tmp -> data.db
+      filename: path.join(process.cwd(), '.tmp', 'data.db'),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
